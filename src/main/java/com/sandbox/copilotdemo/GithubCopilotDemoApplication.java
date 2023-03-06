e99f258e48b08ea6697e86a6ca81a4a9c1af8b6b
ashishsinha1103 <ashish.sinha1103@gmail.com>
2023-03-06 17:16:32 +0530

    first commit

---
+++ src/main/java/com/sandbox/copilotdemo/GithubCopilotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.copilotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCopilotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCopilotDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandbox/copilotdemo/GithubCopilotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.copilotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCopilotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
